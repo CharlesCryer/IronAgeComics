9c7217ba8522fed2ed74358eb26b55a73b1f9d87
CharlesCryer <161407816+CharlesCryer@users.noreply.github.com>
2025-02-08 13:41:30 +1300

    add temporary log statement to debug production

--- src/server/api/routers/comic.ts
+++ src/server/api/routers/comic.ts
@@ -3,10 +3,13 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { comics } from "@/server/db/schema";
 import { like } from "drizzle-orm";
+import { log } from "console";
 
 export const comicRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.db.query.comics.findMany();
+    const comic = await ctx.db.query.comics.findMany();
+    log(comic);
+    return comic;
   }),
   search: publicProcedure.input(z.string()).query(async ({ ctx, input }) => {
     return await ctx.db
