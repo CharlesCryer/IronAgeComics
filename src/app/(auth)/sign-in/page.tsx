b9d9e767bc07cdff980a4aa55f3e96ad83578dac
CharlesCryer <161407816+CharlesCryer@users.noreply.github.com>
2025-02-09 17:50:03 +1300

    testing out error

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -56,7 +56,7 @@ export default function SignIn() {
           form.reset();
         },
         onError: (ctx) => {
-          alert(ctx.error.message);
+          alert(ctx.error);
           setIsSubmitting(false);
         },
       },
